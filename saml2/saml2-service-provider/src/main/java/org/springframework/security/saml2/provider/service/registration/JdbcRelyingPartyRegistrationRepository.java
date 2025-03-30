d8e71a95928f1982db1a779132facbd6c13f9adc
chao.wang <chao.wang@zatech.com>
2025-03-30 22:04:24 +0800

    Add JdbcRelyingPartyRegistrationRepository and database schema for SAML2 relying party registration

--- saml2/saml2-service-provider/src/main/java/org/springframework/security/saml2/provider/service/registration/JdbcRelyingPartyRegistrationRepository.java
+++ saml2/saml2-service-provider/src/main/java/org/springframework/security/saml2/provider/service/registration/JdbcRelyingPartyRegistrationRepository.java
@@ -16,21 +16,25 @@
 
 package org.springframework.security.saml2.provider.service.registration;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.Registration;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+import java.security.interfaces.RSAPrivateKey;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 import java.sql.Types;
-import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
-import java.util.function.Function;
 
-import org.springframework.dao.DataRetrievalFailureException;
+import java.util.function.Consumer;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.ArgumentPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.PreparedStatementSetter;
@@ -39,15 +43,12 @@ import org.springframework.jdbc.core.SqlParameterValue;
 import org.springframework.jdbc.support.lob.DefaultLobHandler;
 import org.springframework.jdbc.support.lob.LobCreator;
 import org.springframework.jdbc.support.lob.LobHandler;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2RefreshToken;
-import org.springframework.security.saml2.provider.service.registration.IterableRelyingPartyRegistrationRepository;
-import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
+import org.springframework.security.saml2.core.Saml2X509Credential;
 import org.springframework.util.Assert;
-import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 public class JdbcRelyingPartyRegistrationRepository implements IterableRelyingPartyRegistrationRepository {
@@ -106,7 +107,8 @@ public class JdbcRelyingPartyRegistrationRepository implements IterableRelyingPa
 	/**
 	 * Constructs a {@code JdbcOAuth2AuthorizedClientService} using the provided
 	 * parameters.
-	 * @param jdbcOperations the JDBC operations
+	 *
+	 * @param jdbcOperations               the JDBC operations
 	 * @param clientRegistrationRepository the repository of client registrations
 	 */
 	public JdbcRelyingPartyRegistrationRepository(JdbcOperations jdbcOperations) {
@@ -116,13 +118,14 @@ public class JdbcRelyingPartyRegistrationRepository implements IterableRelyingPa
 	/**
 	 * Constructs a {@code JdbcOAuth2AuthorizedClientService} using the provided
 	 * parameters.
-	 * @param jdbcOperations the JDBC operations
+	 *
+	 * @param jdbcOperations               the JDBC operations
 	 * @param clientRegistrationRepository the repository of client registrations
-	 * @param lobHandler the handler for large binary fields and large text fields
+	 * @param lobHandler                   the handler for large binary fields and large text fields
 	 * @since 5.5
 	 */
 	public JdbcRelyingPartyRegistrationRepository(JdbcOperations jdbcOperations,
-												   LobHandler lobHandler) {
+			LobHandler lobHandler) {
 		Assert.notNull(jdbcOperations, "jdbcOperations cannot be null");
 		Assert.notNull(lobHandler, "lobHandler cannot be null");
 		this.jdbcOperations = jdbcOperations;
@@ -136,8 +139,9 @@ public class JdbcRelyingPartyRegistrationRepository implements IterableRelyingPa
 	 * Sets the {@link RowMapper} used for mapping the current row in
 	 * {@code java.sql.ResultSet} to {@link OAuth2AuthorizedClient}. The default is
 	 * {@link OAuth2AuthorizedClientRowMapper}.
+	 *
 	 * @param authorizedClientRowMapper the {@link RowMapper} used for mapping the current
-	 * row in {@code java.sql.ResultSet} to {@link OAuth2AuthorizedClient}
+	 *                                  row in {@code java.sql.ResultSet} to {@link OAuth2AuthorizedClient}
 	 */
 	public final void setAuthorizedClientRowMapper(RowMapper<RelyingPartyRegistration> authorizedClientRowMapper) {
 		Assert.notNull(authorizedClientRowMapper, "authorizedClientRowMapper cannot be null");
@@ -152,8 +156,8 @@ public class JdbcRelyingPartyRegistrationRepository implements IterableRelyingPa
 	@Override
 	public RelyingPartyRegistration findByRegistrationId(String registrationId) {
 		Assert.hasText(registrationId, "registrationId cannot be empty");
-		SqlParameterValue[] parameters = new SqlParameterValue[] {
-				new SqlParameterValue(Types.VARCHAR, registrationId) };
+		SqlParameterValue[] parameters = new SqlParameterValue[]{
+				new SqlParameterValue(Types.VARCHAR, registrationId)};
 		PreparedStatementSetter pss = new ArgumentPreparedStatementSetter(parameters);
 		List<RelyingPartyRegistration> result = this.jdbcOperations.query(LOAD_RP_REGISTRATION_SQL, pss,
 				this.authorizedClientRowMapper);
@@ -168,6 +172,8 @@ public class JdbcRelyingPartyRegistrationRepository implements IterableRelyingPa
 
 		protected LobHandler lobHandler = new DefaultLobHandler();
 
+		private ObjectMapper objectMapper = new ObjectMapper();
+
 		public final void setLobHandler(LobHandler lobHandler) {
 			Assert.notNull(lobHandler, "lobHandler cannot be null");
 			this.lobHandler = lobHandler;
@@ -175,8 +181,194 @@ public class JdbcRelyingPartyRegistrationRepository implements IterableRelyingPa
 
 		@Override
 		public RelyingPartyRegistration mapRow(ResultSet rs, int rowNum) throws SQLException {
+			String id = rs.getString("id");
+			String entityId = rs.getString("entity_id");
+			String nameIdFormat = rs.getString("name_id_format");
+			String acsLocation = rs.getString("acs_location");
+			String acsBinding = rs.getString("acs_binding");
+			Credentials signingCredentials = parseCredentials(getLobValue(rs, "signing_credentials"));
+			Credentials decryptionCredentials = parseCredentials(getLobValue(rs, "decryption_credentials"));
+			String singleLogoutUrl = rs.getString("singlelogout_url");
+			String singleLogoutResponseUrl = rs.getString("singlelogout_response_url");
+			String singleLogoutBinding = rs.getString("singlelogout_binding");
+			String assertingPartyEntityId = rs.getString("assertingparty_entity_id");
+			String assertingPartyMetadataUri = rs.getString("assertingparty_metadata_uri");
+			String assertingPartySingleSignOnUrl = rs.getString("assertingparty_singlesignon_url");
+			Saml2MessageBinding assertingPartySingleSignOnBinding = Saml2MessageBinding.valueOf(rs.getString("assertingparty_singlesignon_binding"));
+			Boolean assertingPartySingleSignOnSignRequest = rs.getBoolean("assertingparty_singlesignon_sign_request");
+			Credentials assertingPartyVerificationCredentials = parseCredentials(getLobValue(rs, "assertingparty_verification_credentials"));
+			String assertingPartySingleLogoutUrl = rs.getString("assertingparty_singlelogout_url");
+			String assertingPartySingleLogoutResponseUrl = rs.getString("assertingparty_singlelogout_response_url");
+			Saml2MessageBinding assertingPartySingleLogoutBinding = Saml2MessageBinding.valueOf(rs.getString("assertingparty_singlelogout_binding"));
 
-			return new OAuth2AuthorizedClient(clientRegistration, principalName, accessToken, refreshToken);
+			boolean usingMetadata = StringUtils.hasText(assertingPartyMetadataUri);
+			RelyingPartyRegistration.Builder builder = (!usingMetadata) ? RelyingPartyRegistration.withRegistrationId(id)
+					: createBuilderUsingMetadata(assertingPartyEntityId, assertingPartyMetadataUri).registrationId(id);
+			builder.assertionConsumerServiceLocation(acsLocation);
+			builder.assertionConsumerServiceBinding(Saml2MessageBinding.valueOf(acsBinding));
+			builder.assertingPartyMetadata(mapAssertingParty(assertingPartyEntityId, assertingPartySingleSignOnBinding, assertingPartySingleSignOnUrl, assertingPartySingleSignOnSignRequest,
+					assertingPartySingleLogoutBinding, assertingPartySingleLogoutResponseUrl, assertingPartySingleLogoutUrl));
+			builder.signingX509Credentials((credentials) -> signingCredentials
+					.getCredentials()
+					.stream()
+					.map(this::asSigningCredential)
+					.forEach(credentials::add));
+			builder.decryptionX509Credentials((credentials) -> properties.getDecryption()
+					.getCredentials()
+					.stream()
+					.map(this::asDecryptionCredential)
+					.forEach(credentials::add));
+			builder.assertingPartyMetadata(
+					(details) -> details.verificationX509Credentials((credentials) -> properties.getAssertingparty()
+							.getVerification()
+							.getCredentials()
+							.stream()
+							.map(this::asVerificationCredential)
+							.forEach(credentials::add)));
+			builder.singleLogoutServiceLocation(properties.getSinglelogout().getUrl());
+			builder.singleLogoutServiceResponseLocation(properties.getSinglelogout().getResponseUrl());
+			builder.singleLogoutServiceBinding(properties.getSinglelogout().getBinding());
+			builder.entityId(properties.getEntityId());
+			builder.nameIdFormat(properties.getNameIdFormat());
+			RelyingPartyRegistration registration = builder.build();
+			boolean signRequest = registration.getAssertingPartyMetadata().getWantAuthnRequestsSigned();
+			validateSigningCredentials(properties, signRequest);
+			return registration;
+		}
+
+		private Saml2X509Credential asSigningCredential(Signing.Credential properties) {
+			RSAPrivateKey privateKey = readPrivateKey(properties.getPrivateKeyLocation());
+			X509Certificate certificate = readCertificate(properties.getCertificateLocation());
+			return new Saml2X509Credential(privateKey, certificate, Saml2X509Credential.Saml2X509CredentialType.SIGNING);
+		}
+
+		private RSAPrivateKey readPrivateKey(Resource location) {
+			Assert.state(location != null, "No private key location specified");
+			Assert.state(location.exists(), () -> "Private key location '" + location + "' does not exist");
+			try (InputStream inputStream = location.getInputStream()) {
+				PemContent pemContent = PemContent.load(inputStream);
+				PrivateKey privateKey = pemContent.getPrivateKey();
+				Assert.isInstanceOf(RSAPrivateKey.class, privateKey,
+						"PrivateKey in resource '" + location + "' must be an RSAPrivateKey");
+				return (RSAPrivateKey) privateKey;
+			}
+			catch (Exception ex) {
+				throw new IllegalArgumentException(ex);
+			}
+		}
+
+		private X509Certificate readCertificate(Resource location) {
+			Assert.state(location != null, "No certificate location specified");
+			Assert.state(location.exists(), () -> "Certificate  location '" + location + "' does not exist");
+			try (InputStream inputStream = location.getInputStream()) {
+				PemContent pemContent = PemContent.load(inputStream);
+				List<X509Certificate> certificates = pemContent.getCertificates();
+				return certificates.get(0);
+			}
+			catch (Exception ex) {
+				throw new IllegalArgumentException(ex);
+			}
+		}
+
+		private Consumer<AssertingPartyMetadata.Builder<?>> mapAssertingParty(String assertingPartyEntityId, Saml2MessageBinding assertingPartySingleSignOnBinding, String assertingPartySingleSignOnUrl,
+				Boolean assertingPartySingleSignOnSignRequest, Saml2MessageBinding assertingPartySingleLogoutBinding, String assertingPartySingleLogoutResponseUrl, String assertingPartySingleLogoutUrl) {
+			return (details) -> {
+				details.entityId(assertingPartyEntityId);
+				details.singleSignOnServiceBinding(assertingPartySingleSignOnBinding);
+				details.singleSignOnServiceLocation(assertingPartySingleSignOnUrl);
+				details.wantAuthnRequestsSigned(assertingPartySingleSignOnSignRequest);
+				details.singleLogoutServiceLocation(assertingPartySingleLogoutUrl);
+				details.singleLogoutServiceResponseLocation(assertingPartySingleLogoutResponseUrl);
+				details.singleLogoutServiceBinding(assertingPartySingleLogoutBinding);
+			};
+		}
+
+		private RelyingPartyRegistration.Builder createBuilderUsingMetadata(String assertingPartyEntityId, String assertingPartyMetadataUri) {
+			Collection<RelyingPartyRegistration.Builder> candidates = RelyingPartyRegistrations
+					.collectionFromMetadataLocation(assertingPartyMetadataUri);
+			for (RelyingPartyRegistration.Builder candidate : candidates) {
+				if (assertingPartyEntityId == null || assertingPartyEntityId.equals(getEntityId(candidate))) {
+					return candidate;
+				}
+			}
+			throw new IllegalStateException("No relying party with Entity ID '" + assertingPartyEntityId + "' found");
+		}
+
+		private Credentials parseCredentials(String signingCredentials) {
+			try {
+				return this.objectMapper.readValue(signingCredentials, new TypeReference<>() {
+				});
+			}
+			catch (Exception ex) {
+				throw new IllegalArgumentException(ex.getMessage(), ex);
+			}
+		}
+
+		private String getLobValue(ResultSet rs, String columnName) throws SQLException {
+			String columnValue = null;
+			byte[] columnValueBytes = this.lobHandler.getBlobAsBytes(rs, columnName);
+			if (columnValueBytes != null) {
+				columnValue = new String(columnValueBytes, StandardCharsets.UTF_8);
+			}
+			return columnValue;
+		}
+
+		public static class Credentials {
+
+			/**
+			 * Credentials used for signing the SAML authentication request.
+			 */
+			private List<Credential> credentials = new ArrayList<>();
+
+			public List<Credential> getCredentials() {
+				return this.credentials;
+			}
+
+			public void setCredentials(List<Credential> credentials) {
+				this.credentials = credentials;
+			}
+
+			public static class Credential {
+
+				/**
+				 * Private key used for signing.
+				 */
+				private String privateKey;
+
+				/**
+				 * Relying Party X509Certificate shared with the identity provider.
+				 */
+				private String certificate;
+
+				public String getPrivateKey() {
+					return privateKey;
+				}
+
+				public void setPrivateKey(String privateKey) {
+					this.privateKey = privateKey;
+				}
+
+				public String getCertificate() {
+					return certificate;
+				}
+
+				public void setCertificate(String certificate) {
+					this.certificate = certificate;
+				}
+			}
+
+		}
+
+		private Object getEntityId(RelyingPartyRegistration.Builder candidate) {
+			String[] result = new String[1];
+			candidate.assertingPartyMetadata((builder) -> result[0] = builder.build().getEntityId());
+			return result[0];
+		}
+
+
+
+		private RelyingPartyRegistration asRegistration(String id, Registration properties) {
+
 		}
 
 	}
